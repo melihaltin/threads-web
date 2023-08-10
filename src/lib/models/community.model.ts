83641f228540c92a2fe754afed000c798f3ccf00
Melih ALTIN <melihaltindev@gmail.com>
2023-08-10 17:47:15 +0300

    community section added

---
+++ src/lib/models/community.model.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const communitySchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  image: String,
+  bio: String,
+  creadtedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  members: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const Community =
+  mongoose.models.Community || mongoose.model("Community", communitySchema);
+
+export default Community;
